a8496c1fb8a357642ed87d6c6ef15915991c2412
One-HyeWon <5876675@naver.com>
2024-04-10 21:53:53 +0900

    :sparkles:[feat]: shuffle() 함수 작성

--- src/App.js
+++ src/App.js
@@ -56,7 +56,15 @@ class App {
   }
 
   shuffle(array) {
-    return;
+    const shuffledArray = [...array];
+    for (let i = shuffledArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffledArray[i], shuffledArray[j]] = [
+        shuffledArray[j],
+        shuffledArray[i],
+      ];
+    }
+    return shuffledArray;
   }
 
   printResult(pairs) {
