d26ddb55dfe82de37045368304804bafcee32899
Jiwon Kim <jiwonkim0531@gachon.ac.kr>
2024-04-11 20:39:47 +0900

    test 수정

--- src/App.js
+++ src/App.js
@@ -8,40 +8,57 @@ class App {
       const names = await this.getInputNames();
       const maxPairs = await this.getMaxPairs();
     
-      const pairs = this.createPairs(names, maxPairs);
-      this.printResult(pairs);
-    
-      MyUtils.Console.print("추천을 완료했습니다.");
+
+      do {
+        const pairs = this.createPairs(names, maxPairs);
+
+        this.printResult(pairs);
+      
+        MyUtils.Console.print("추천을 완료했습니다.");
+        MyUtils.Console.print("다시 구성하시겠습니까? (y or n)");
   
-      await MyUtils.Console.print("다시 구성하시겠습니까? (y or n)");
-      const again = await MyUtils.Console.readLineAsync();
-    
-      if (again !== undefined && again.trim().toLowerCase() === 'y') {
-        await this.play();
-      } else if (again !== undefined && again.trim().toLowerCase() === 'n'){
-        MyUtils.Console.print("자리를 이동해 서로에게 인사해주세요.");
-      } else{
-        throw new Error(`[ERROR] ${error.message}`);
-      }
+        const again = await MyUtils.Console.readLineAsync();
+      
+        if (again !== 'y' && again !== 'n') {
+          throw new Error(`[ERROR] ${error.message}`);
+        }
+        if (again.trim().toLowerCase() === 'n'){
+          MyUtils.Console.print("자리를 이동해 서로에게 인사해주세요.");
+          return;
+        } 
+        if (again.trim().toLowerCase() === 'y') {
+          MyUtils.Console.print("--------------------------------");
+          continue;
+        } 
+      } while (true)
+      return pairs;
     }
     catch (error) {
       throw new Error(`[ERROR] ${error.message}`);
-    }
-    
+    } 
+
+  }
+   
+  async returnAgian(){
+
   }
 
   async getInputNames() {
-    MyUtils.Console.print("멤버의 이름을 입력해 주세요. (, 로 구분): ");
-    const input = await MyUtils.Console.readLineAsync();
-    return input.split(",").map(name => name.trim());
+    MyUtils.Console.print("멤버의 이름을 입력해주세요. (, 로 구분): ");
+    const userInput = await MyUtils.Console.readLineAsync();
+  
+    const hangulRange = /^[가-힣][가-힣,]+[^,]$/;
+    if (!hangulRange.test(userInput)) {
+      throw new Error("[ERROR] 이름은 한글로 입력해야 합니다.");
+    }
+  
+    return userInput.split(",").map(name => name.trim());
   }
+  
 
   async getMaxPairs() {
-    MyUtils.Console.print("최대 짝 수를 입력해 주세요.: ");
+    MyUtils.Console.print("최대 짝 수를 입력해주세요.: ");
     const maxPairs = await MyUtils.Console.readLineAsync();
-    if (isNaN(maxPairsInt) || maxPairsInt <= 0) {
-      throw new Error ("최대 짝 수는 양의 정수여야 합니다.");
-    }
     return parseInt(maxPairs);
   }
 
@@ -68,6 +85,8 @@ class App {
       MyUtils.Console.print(`[ ${pair.join(" | ")} ]`);
     });
   }
+
+
 }
 
 module.exports = App;
